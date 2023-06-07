fc9b4d068a849cc1d7a49c48ab073f5bd8bd6822
bobs3400 <124318533+bobs3400@users.noreply.github.com>
2023-06-07 21:12:54 +1000

    40-1

--- DZ40.py
+++ DZ40.py
@@ -1,1 +1,7 @@
-num = int(input())
+first_elem = int(input("Введите первый элемент: "))
+difference = int(input("Введите разность: "))
+num_elem = int(input("Введите количество элементов: "))
+array = [first_elem]
+for i in range(2, num_elem + 1):
+    array.append(first_elem + (i - 1) * difference)
+print(array)
